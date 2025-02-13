07266c2943faefe11fb2df76fac62ad393c061cc
Atik-Sahariyar <atiksahariyar88@gmail.com>
2025-02-13 13:14:00 +0600

    update login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,27 +2,31 @@ import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../../redux/features/auth/authApi";
 import { toast } from "sonner";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
+  const [ isShowPass, setIsShowPass] = useState(false);
   const { register, handleSubmit, reset } = useForm();
   const navigate  = useNavigate();
   const [ login ] = useLoginMutation();
 
   // handle login user
- const handleLoginWithEmailPass = async(data) =>{
+ const handleLoginWithEmailPass = async(data: { email: string, password: string}) =>{
    try{
-    const toastId = toast.loading('Logging in');
+    const toastId  = toast.loading('Logging in');
 
      const res = await login(data);
      console.log(res);
      if(res?.data?.success){
         toast.success(res?.data?.message , { id: toastId, duration: 2000 });
+        reset();
+        navigate("/");
      } else if (res?.error){
         toast.error(res?.error?.data?.message , { id: toastId, duration: 2000 });
         console.log(res?.error?.data?.message)
      }
-    //  reset();
-    //  navigate("/")
+
    } catch(err){
     console.log(err)
     toast.error(err?.message,  { id: toastId, duration: 2000 })
@@ -59,19 +63,29 @@ const Login = () => {
             </div>
 
             {/* Password Field */}
-            <div className="mb-4">
+            <div className="mb-4 ">
               <label
                 htmlFor="password"
                 className="block text-gray-700 font-medium mb-2"
               >
                 Password
               </label>
+              <div className=" relative">
               <input
-                type="password"
+                type={isShowPass ? "text" : "password"}
                 {...register("password", { required:true})}
                 placeholder="Enter your password"
                 className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
               />
+               
+               <button 
+               onClick={() => setIsShowPass(!isShowPass)}
+               className=" absolute right-5 top-3 z-20 text-xl opacity-60 hover:opacity-80" >
+                 {
+                  isShowPass ?   <FaEyeSlash /> : <FaEye />
+                 }
+               </button>
+              </div>
             </div>
 
             {/* Remember Me & Forgot Password */}
